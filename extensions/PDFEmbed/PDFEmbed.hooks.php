b0c9a297890769e934d0d957dd8a9efce4f2fd51
Alexia E. Smith <washuu@gmail.com>
2013-09-25 17:10:51 -0500

    Initial import of PDFEmbed

---
+++ extensions/PDFEmbed/PDFEmbed.hooks.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * PDFEmbed
+ * PDFEmbed Hooks
+ *
+ * @author		Alexia E. Smith
+ * @license		LGPLv3 http://opensource.org/licenses/lgpl-3.0.html
+ * @package		PDFEmbed
+ * @link		http://www.mediawiki.org/wiki/Extension:PDFEmbed
+ *
+ **/
+
+class PDFEmbed {
+	/**
+	 * Sets up this extensions parser functions.
+	 *
+	 * @access	public
+	 * @param	object	Parser object passed as a reference.
+	 * @return	boolean	true
+	 */
+	static public function onParserFirstCallInit(Parser &$parser) {
+		$parser->setHook('pdf', 'PDFEmbed::generateTag');
+
+		return true;
+	}
+
+	/**
+	 * Generates the PDF object tag.
+	 *
+	 * @access	public
+	 * @param	string	Namespace prefixed article of the PDF file to display.
+	 * @param	array	Arguments on the tag.
+	 * @param	object	Parser object.
+	 * @param	object	PPFrame object.
+	 * @return	string	HTML
+	 */
+	static public function generateTag($file, $args = array(), Parser $parser, PPFrame $frame) {
+		global $pdfEmbed, $wgRequest, $wgUser;
+		$parser->disableCache();
+
+		if ($wgRequest->getVal('action') == 'edit' || $wgRequest->getVal('action') == 'submit') {
+			$user = $wgUser;
+		} else {
+			$user = User::newFromName($parser->getRevisionUser());
+		}
+
+		if ($user === false) {
+			return self::error('embed_pdf_invalid_user');
+		}
+
+		if (!$user->isAllowed('embed_pdf')) {
+			return self::error('embed_pdf_no_permission');
+		}
+
+		if (empty($file) || !preg_match('#(.+?)\.pdf#is', $file)) {
+			return self::error('embed_pdf_blank_file');
+		}
+
+		$file = wfFindFile(Title::newFromText($file));
+
+		$width  = ($args['width'] > 0 ? intval($args['width']) : intval($pdfEmbed['width']));
+		$height = ($args['height'] > 0 ? intval($args['height']) : intval($pdfEmbed['height']));
+
+		if ($file !== false) {
+			return self::embed($file, $width, $height);
+		} else {
+			return self::error('embed_pdf_invalid_file');
+		}
+	}
+
+	/**
+	 * Returns a standard error message.
+	 *
+	 * @access	public
+	 * @param	object	File object.
+	 * @param	integer	Width of the object.
+	 * @param	integer	Height of the object.
+	 * @return	string	HTML object.
+	 */
+	static private function embed(File $file, $width, $height) {
+		return "<object width='{$width}' height='{$height}' data='".$file->getFullUrl()."' type='application/pdf'>".wfMessage('pdf_not_supported', $file->getFullUrl(), $file->getName())->plain()."</object>";
+	}
+
+	/**
+	 * Returns a standard error message.
+	 *
+	 * @access	public
+	 * @param	string	Error message key to display.
+	 * @return	string	HTML error message.
+	 */
+	static private function error($messageKey) {
+		return Xml::span(wfMessage($messageKey)->plain(), 'error');
+	}
+}
+?>
---
+++ extensions/PDFEmbed/PDFEmbed.i18n.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PDFEmbed
+ * PDFEmbed Language Strings
+ *
+ * @author		Alexia E. Smith
+ * @license		LGPLv3 http://opensource.org/licenses/lgpl-3.0.html
+ * @package		PDFEmbed
+ * @link		http://www.mediawiki.org/wiki/Extension:PDFEmbed
+ *
+ **/
+
+$messages = array();
+
+$messages['en'] = [
+	'pdfembed'						=> 'PDF Embed',
+	'pdfembed_description'			=> 'Media handler extension for PDF files.',
+	'embed_pdf_no_permission'		=> 'You do not have permission to embed PDF files.',
+	'embed_pdf_blank_file'			=> 'The URL or file path given to embed a PDF is blank.',
+	'embed_pdf_invalid_user'		=> 'An invalid user was specified to permission testing to embed this PDF.',
+	'pdf_not_supported'				=> 'The PDF file did not load properly or your web browser does not support viewing PDF files.  Download <a href="$1">$2</a> directly to your device.'
+];
+
+$magicWords = array();
+
+$magicWords['en'] = array();
+
+?>
