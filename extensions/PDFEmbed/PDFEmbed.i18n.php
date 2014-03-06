a86f90a89d08e82283ef9e10790c95f56125b18e
George Barnick <george.barnick@brickcraft.me>
2014-03-06 00:48:00 -0500

    Fixing a syntax error in i18n file

--- extensions/PDFEmbed/PDFEmbed.i18n.php
+++ extensions/PDFEmbed/PDFEmbed.i18n.php
@@ -12,14 +12,14 @@
 
 $messages = array();
 
-$messages['en'] = [
+$messages['en'] = array(
 	'pdfembed'						=> 'PDF Embed',
 	'pdfembed_description'			=> 'Media handler extension for PDF files.',
 	'embed_pdf_no_permission'		=> 'You do not have permission to embed PDF files.',
 	'embed_pdf_blank_file'			=> 'The URL or file path given to embed a PDF is blank.',
 	'embed_pdf_invalid_user'		=> 'An invalid user was specified to permission testing to embed this PDF.',
 	'pdf_not_supported'				=> 'The PDF file did not load properly or your web browser does not support viewing PDF files.  Download <a href="$1">$2</a> directly to your device.'
-];
+);
 
 $magicWords = array();
 
